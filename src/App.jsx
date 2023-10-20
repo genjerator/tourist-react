60f369eacf6895220722e79c85c2f65787f9bf4f
evgenije <evgenije.medjesi@get-e.com>
2023-10-20 14:40:43 +0200

    amplify

--- src/App.jsx
+++ src/App.jsx
@@ -43,6 +43,7 @@ function App() {
                         <Link to="/login" className="text-white hover:text-gray-300 mr-4">Login</Link>
                         <Link to="/register" className="text-white hover:text-gray-300 mr-4">Register</Link>
                         <button type="button" onClick={handleLogout} className="text-white hover:text-gray-300 mr-4">Logout</button>
+                        <p>aaaaa</p>
                     </div>
                 </div>
             </nav>
